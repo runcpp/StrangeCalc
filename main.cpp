bda325da565901ad3b8e085eb8ea3776782477c9
runcpp <grzegorz.runc@gmail.com>
2019-10-15 12:17:19 +0200

    DecimalFraction operations: plus, minus, times, by added.

--- main.cpp
+++ main.cpp
@@ -18,92 +18,58 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEAL
 */
 
 #include <iostream>
-#include <memory>
 #include <string>
-#include <utility>
-
-class MathematicalConstruct
-{
-public:
-    virtual void print(std::ostream& os) const = 0;
-};
-
-class Constant: public MathematicalConstruct
-{
-private:
-    long double value;
-    std::string name;
-
-public:
-    explicit Constant(std::string  name, long double value);
-    void print(std::ostream& os) const override;
-};
-
-void Constant::print(std::ostream& os) const
-{
-    os << value;
-}
-
-Constant::Constant(std::string  name, const long double value) : value(value), name(std::move(name))
-{
-
-}
-
-class Equation: public MathematicalConstruct
-{
-private:
-    std::unique_ptr<MathematicalConstruct> child;
-public:
-    explicit Equation(MathematicalConstruct& operation);
-    explicit Equation(MathematicalConstruct&& operation);
-    friend std::ostream& operator << (std::ostream& os, const Equation& eq);
-    void print(std::ostream& os) const override;
-};
-
-void Equation::print(std::ostream &os) const
-{
-    if (child)
-    {
-        child->print(os);
-    }
-}
-
-Equation::Equation(MathematicalConstruct &operation) : child(&operation)
-{}
-
-Equation::Equation(MathematicalConstruct &&operation) : child(&operation)
-{}
-
-std::ostream& operator<<(std::ostream& os, const Equation& eq)
-{
-    eq.print(os);
-
-    return os;
-}
 
 class DecimalFraction
 {
 private:
     long long numerator;
     long long denominator;
+    static long long GCD(long long a, long long b);
     [[nodiscard]] long long LCM(const DecimalFraction& rhs) const;
     [[nodiscard]] long long GCD(const DecimalFraction& rhs) const;
-    long long fractionGCD() const;
+    [[nodiscard]] long long fractionGCD() const;
 
 public:
     DecimalFraction(long long numerator, long long denominator);
     DecimalFraction operator + (const DecimalFraction& rhs) const;
     DecimalFraction operator + (long long rhs) const;
-    friend std::ostream& operator << (std::ostream& os, const DecimalFraction& fraction);
+
     friend DecimalFraction operator + (long long lhs, DecimalFraction& rhs);
     friend DecimalFraction operator + (long long lhs, DecimalFraction&& rhs);
 
-    void recduce();
+    DecimalFraction operator - (const DecimalFraction& rhs) const;
+    DecimalFraction operator - (long long rhs) const;
+
+    friend DecimalFraction operator - (long long lhs, DecimalFraction& rhs);
+    friend DecimalFraction operator - (long long lhs, DecimalFraction&& rhs);
+
+    DecimalFraction operator * (const DecimalFraction& rhs) const;
+    DecimalFraction operator * (long long rhs) const;
+
+    friend DecimalFraction operator * (long long lhs, DecimalFraction& rhs);
+    friend DecimalFraction operator * (long long lhs, DecimalFraction&& rhs);
+
+    DecimalFraction operator / (const DecimalFraction& rhs) const;
+    DecimalFraction operator / (long long rhs) const;
+
+    friend DecimalFraction operator / (long long lhs, DecimalFraction& rhs);
+    friend DecimalFraction operator / (long long lhs, DecimalFraction&& rhs);
+
+    friend std::ostream& operator << (std::ostream& os, const DecimalFraction& fraction);
+
+    void reduce();
 };
 
 DecimalFraction::DecimalFraction(long long numerator, long long denominator):
         numerator(numerator),denominator(denominator)
-{}
+{
+    if (denominator < 0)
+    {
+        this->denominator = -denominator;
+        this->numerator = -numerator;
+    }
+}
 
 DecimalFraction DecimalFraction::operator+(const DecimalFraction &rhs) const
 {
@@ -117,7 +83,7 @@ DecimalFraction DecimalFraction::operator+(const DecimalFraction &rhs) const
 
 
     DecimalFraction value(lhsNumerator + rhsNumerator, lcm);
-    value.recduce();
+    value.reduce();
 
     return value;
 }
@@ -129,46 +95,102 @@ DecimalFraction DecimalFraction::operator+(const long long rhs) const
 
 std::ostream& operator<<(std::ostream& os, const DecimalFraction& fraction)
 {
-    os << fraction.numerator << "/" << fraction.denominator;
+    os << fraction.numerator;
+    if (fraction.denominator != 1)
+    {
+        os << "/" << fraction.denominator;
+    }
 
     return os;
 }
 
-long long DecimalFraction::LCM(const DecimalFraction &rhs) const {
+long long DecimalFraction::LCM(const DecimalFraction &rhs) const
+{
     return this->denominator*rhs.denominator/GCD(rhs);
 }
 
-long long DecimalFraction::GCD(const DecimalFraction &rhs) const {
-    long long a = denominator;
-    long long b = rhs.denominator;
-
-    while(a != b)
-    {
-        if (a < b)
-        {
-            b -= a;
-        }
-        else
-        {
-            a -= b;
-        }
-    }
-
-    return a;
+long long DecimalFraction::GCD(const DecimalFraction &rhs) const
+{
+    return DecimalFraction::GCD(denominator, rhs.denominator);
 }
 
-void DecimalFraction::recduce()
+void DecimalFraction::reduce()
 {
     long long gcd = fractionGCD();
     denominator /= gcd;
     numerator /= gcd;
 }
 
-long long DecimalFraction::fractionGCD() const {
-    long long a = denominator;
-    long long b = numerator;
+long long DecimalFraction::fractionGCD() const
+{
+    long long a = llabs(denominator);
+    long long b = llabs(numerator);
 
-    while (a != b)
+    return DecimalFraction::GCD(a, b);
+}
+
+DecimalFraction operator+(long long lhs, DecimalFraction &rhs)
+{
+    return rhs + lhs;
+}
+
+DecimalFraction operator+(long long lhs, DecimalFraction &&rhs)
+{
+    return rhs + lhs;
+}
+
+DecimalFraction DecimalFraction::operator-(const DecimalFraction &rhs) const
+{
+    //Common denominator
+    long long lcm = LCM(rhs);
+    long long lhsDenominator = denominator;
+    long long rhsDenominator = rhs.denominator;
+
+    long long lhsNumerator = numerator * lcm / lhsDenominator;
+    long long rhsNumerator = rhs.numerator * lcm / rhsDenominator;
+
+
+    DecimalFraction value(lhsNumerator - rhsNumerator, lcm);
+    value.reduce();
+
+    return value;
+}
+
+DecimalFraction DecimalFraction::operator-(long long rhs) const
+{
+    return *this - DecimalFraction(rhs, 1);
+}
+
+DecimalFraction operator-(long long lhs, DecimalFraction &rhs)
+{
+    return DecimalFraction(lhs, 1) - rhs;
+}
+
+DecimalFraction operator-(long long lhs, DecimalFraction &&rhs)
+{
+    return DecimalFraction(lhs, 1) - rhs;
+}
+
+DecimalFraction DecimalFraction::operator*(const DecimalFraction &rhs) const
+{
+    //Common denominator
+    long long gcd1 = DecimalFraction::GCD(llabs(numerator), llabs(rhs.denominator));
+    long long lhsNumerator = numerator / gcd1;
+    long long rhsDenominator = rhs.denominator / gcd1;
+
+    long long gcd2 = DecimalFraction::GCD(denominator, rhs.numerator);
+    long long rhsNumerator = rhs.numerator / gcd2;
+    long long lhsDenominator = denominator / gcd2;
+
+    DecimalFraction value(lhsNumerator * rhsNumerator, lhsDenominator * rhsDenominator);
+    value.reduce();
+
+    return value;
+}
+
+long long DecimalFraction::GCD(long long a, long long b)
+{
+    while (a != b && a >= 1 && b >= 1)
     {
         if (a < b)
         {
@@ -183,18 +205,79 @@ long long DecimalFraction::fractionGCD() const {
     return a;
 }
 
-DecimalFraction operator+(long long lhs, DecimalFraction &rhs) {
-    return rhs + lhs;
+DecimalFraction DecimalFraction::operator*(long long rhs) const
+{
+    return *this * DecimalFraction(rhs, 1);
 }
 
-DecimalFraction operator+(long long lhs, DecimalFraction &&rhs) {
-    return rhs + lhs;
+DecimalFraction operator*(long long lhs, DecimalFraction &rhs)
+{
+    return DecimalFraction(lhs, 1) * rhs;
+}
+
+DecimalFraction operator*(long long lhs, DecimalFraction &&rhs)
+{
+    return DecimalFraction(lhs, 1) * rhs;
+}
+
+DecimalFraction DecimalFraction::operator/(const DecimalFraction &rhs) const
+{
+    return *this * DecimalFraction(rhs.denominator, rhs.numerator);
+}
+
+DecimalFraction DecimalFraction::operator/(long long rhs) const
+{
+    return *this / DecimalFraction(rhs, 1);
+}
+
+DecimalFraction operator/(long long lhs, DecimalFraction &rhs)
+{
+    return DecimalFraction(lhs, 1) / rhs;
+}
+
+DecimalFraction operator/(long long lhs, DecimalFraction &&rhs)
+{
+    return DecimalFraction(lhs, 1) / rhs;
 }
 
 int main()
 {
-    std::cout << 1 + DecimalFraction(1, 3) << std::endl;
-    std::cout << DecimalFraction(1, 3) + DecimalFraction(1, 6) << std::endl;
-    std::cout << DecimalFraction(15, 4) + DecimalFraction(2, 14) << std::endl;
+    DecimalFraction lhs1 = DecimalFraction(-4, 5);
+    long long rhs1 = 2;
+    long long lhs2 = 1;
+    DecimalFraction rhs2 = DecimalFraction(1, 3);
+    DecimalFraction lhs3 = DecimalFraction(1, 3);
+    DecimalFraction rhs3 = DecimalFraction(1, 6);
+    DecimalFraction lhs4 = DecimalFraction(15, 4);
+    DecimalFraction rhs4 = DecimalFraction(2, 14);
+
+    std::cout << "Addition:" << std::endl;
+    std::cout << lhs1 << " + " << rhs1 << " = " << lhs1 + rhs1 << std::endl;
+    std::cout << lhs2 << " + " << rhs2 << " = " << lhs2 + rhs2 << std::endl;
+    std::cout << lhs3 << " + " << rhs3 << " = " << lhs3 + rhs3 << std::endl;
+    std::cout << lhs4 << " + " << rhs4 << " = " << lhs4 + rhs4 << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Subtraction:" << std::endl;
+    std::cout << lhs1 << " - " << rhs1 << " = " << lhs1 - rhs1 << std::endl;
+    std::cout << lhs2 << " - " << rhs2 << " = " << lhs2 - rhs2 << std::endl;
+    std::cout << lhs3 << " - " << rhs3 << " = " << lhs3 - rhs3 << std::endl;
+    std::cout << lhs4 << " - " << rhs4 << " = " << lhs4 - rhs4 << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Multiplication:" << std::endl;
+    std::cout << lhs1 << " * " << rhs1 << " = " << lhs1 * rhs1 << std::endl;
+    std::cout << lhs2 << " * " << rhs2 << " = " << lhs2 * rhs2 << std::endl;
+    std::cout << lhs3 << " * " << rhs3 << " = " << lhs3 * rhs3 << std::endl;
+    std::cout << lhs4 << " * " << rhs4 << " = " << lhs4 * rhs4 << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Division:" << std::endl;
+    std::cout << lhs1 << " / " << rhs1 << " = " << lhs1 / rhs1 << std::endl;
+    std::cout << lhs2 << " / " << rhs2 << " = " << lhs2 / rhs2 << std::endl;
+    std::cout << lhs3 << " / " << rhs3 << " = " << lhs3 / rhs3 << std::endl;
+    std::cout << lhs4 << " / " << rhs4 << " = " << lhs4 / rhs4 << std::endl;
+    std::cout << std::endl;
+
     return 0;
 }
